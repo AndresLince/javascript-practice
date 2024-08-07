398d35856b409e8f2a6235c1b4ba805a487648a3
AndresLince <lynxdarmatron@gmail.com>
2024-08-06 20:41:24 -0500

    Tagged templates WIP

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 let harry = "potter";
 let lotr = "frodo";
 let orwell = "1984";
+let lines = [
+    harry,
+    lotr,
+    orwell
+]
 
-function buildHTML() {
-    console.log('It works');
+function buildHTML(strings, expr) {
+    console.log(strings);
+    console.log(expr);
 }
 
-const result = buildHTML`still working`;
+const result = buildHTML`<li>${lines}</li>`;
